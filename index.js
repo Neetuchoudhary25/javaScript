0c7a6e33fb7fae0fb1eb0d30f5e8d38061374368
Neetuchoudhary25 <choudharyneetu059@gmail.com>
2024-06-08 11:48:56 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,137 @@
+
+// //var
+// var num = 10;
+// {
+//     num=20;
+// }
+// console.log("var",num)
+
+
+
+// //let
+// let num1 = 10
+// {
+//     num1=10
+// }
+// console.log("let",num1)
+
+
+// //const
+// const num2 = 10
+// {
+//     num2 = 10
+// }
+
+// console.log("const" ,num2)
+
+
+
+
+// let num1 = 10;
+// let num2 = 20;
+// // let sum = num1 + num2;
+// // console.log(sum)
+
+// let sum = num1 - num2;
+// console.log(sum)
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+
+
+
+// let  num1 = 20;
+// let num2 = 10;
+// let sum = num1++ + num1 + num1++
+// console.log(num1)
+
+// let num1 = 21;
+// let num2 = 10;
+// let sum = ++num1 + num1 + ++num1
+// console.log(num1)
+
+// let num1=5;
+// let num2 =10;
+// let sum = --num1 + ++num2 +num1-- + num2++
+// console.log(sum);
+// console.log(num1);
+// console.log(num2);
+
+
+
+
+
+
+
+// let checkIn = true;
+// let checkOut = true;
+// console.log(checkIn == checkOut);
+
+
+// let checkIn = 10;
+// let checkOut = "10";
+// console.log(checkIn == checkOut);
+
+
+// let checkIn = 10;
+// let checkOut = "10";
+// console.log(typeof checkIn)
+// console.log(typeof checkOut)
+
+// console.log(checkIn ===checkOut);
+
+
+
+
+// let checkIn = "hello";
+// let checkOut = "10";
+// console.log(checkOut);
+
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let result = (a<b && a < c) ? "a" : (b<c) : "b" : "c" ;
+// console.log(result)
+
+
+
+// let age=5;
+// let status1=(age>18)  ? "I can vote": "I cannot vote";
+// console.log(status1);
+
+
+
+// let num1 = 0;
+// let num2 = 30;
+// let result = (num1 = num2) ? "yes" : "no";
+// // let result = (num1>num2) ? num1 : num2;
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
